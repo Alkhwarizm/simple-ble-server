c392efc62781cec25d0a15c985c9f412f9aebcab
Alkhwarizm <iqbal@trafficsim.co.jp>
2020-01-09 13:59:31 +0900

    Added missing constant

--- gatt.py
+++ gatt.py
@@ -9,12 +9,14 @@ except ImportError:
   import gobject as GObject
 
 BLUEZ_SERVICE_NAME = 'org.bluez'
-GATT_MANAGER_IFACE = 'org.bluez.GattManager1'
 DBUS_OM_IFACE =      'org.freedesktop.DBus.ObjectManager'
 DBUS_PROP_IFACE =    'org.freedesktop.DBus.Properties'
 GATT_SERVICE_IFACE = 'org.bluez.GattService1'
 GATT_CHRC_IFACE =    'org.bluez.GattCharacteristic1'
 GATT_DESC_IFACE =    'org.bluez.GattDescriptor1'
+GATT_MANAGER_IFACE = 'org.bluez.GattManager1'
+LE_ADVERTISING_MANAGER_IFACE = 'org.bluez.LEAdvertisingManager1'
+
 
 class InvalidArgsException(dbus.exceptions.DBusException):
     _dbus_error_name = 'org.freedesktop.DBus.Error.InvalidArgs'
@@ -67,7 +69,7 @@ class Application(dbus.service.Object):
         print("Failed to register GATT application: {}".format(error))
 
     def register(self):
-        adapter: find_adapter(self.bus)
+        adapter = find_adapter(self.bus)
         service_manager = dbus.Interface(
             self.bus.get_object(BLUEZ_SERVICE_NAME, adapter),
             GATT_MANAGER_IFACE
